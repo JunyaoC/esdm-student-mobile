82d75386a0de2fc93f6380e2c604761ca810bb71
Khoo Jie Xuan <coolkhoo547@gmail.com>
2021-05-20 18:03:30 +0800

    Pages creation

--- esdm-student-mobile/src/app/dining/dining-routing.module.ts
+++ esdm-student-mobile/src/app/dining/dining-routing.module.ts
@@ -7,6 +7,34 @@ const routes: Routes = [
   {
     path: '',
     component: DiningPage
+  },
+  {
+    path: 'food',
+    loadChildren: () => import('./food/food.module').then( m => m.FoodPageModule)
+  },
+  {
+    path: 'food-details',
+    loadChildren: () => import('./food-details/food-details.module').then( m => m.FoodDetailsPageModule)
+  },
+  {
+    path: 'cart',
+    loadChildren: () => import('./cart/cart.module').then( m => m.CartPageModule)
+  },
+  {
+    path: 'order',
+    loadChildren: () => import('./order/order.module').then( m => m.OrderPageModule)
+  },
+  {
+    path: 'order-history',
+    loadChildren: () => import('./order-history/order-history.module').then( m => m.OrderHistoryPageModule)
+  },
+  {
+    path: 'track-order',
+    loadChildren: () => import('./track-order/track-order.module').then( m => m.TrackOrderPageModule)
+  },
+  {
+    path: 'personal-info',
+    loadChildren: () => import('./personal-info/personal-info.module').then( m => m.PersonalInfoPageModule)
   }
 ];
 
