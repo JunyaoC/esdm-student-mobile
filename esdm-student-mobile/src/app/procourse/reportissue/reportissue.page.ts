c500e4d7c8b1bd7881d4a1a7ec09a6b4d4e551a0
jia7lim <66741257+jia7lim@users.noreply.github.com>
2021-05-21 04:11:01 +0800

    update procourse

--- esdm-student-mobile/src/app/procourse/reportissue/reportissue.page.ts
+++ esdm-student-mobile/src/app/procourse/reportissue/reportissue.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-reportissue',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReportissuePage implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {
   }
+
+  backProcourse(){
+  	this.router.navigate(['./procourse'])
+  }
+  
 
 }
