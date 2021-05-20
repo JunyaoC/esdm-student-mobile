c0d6b2fef013cc57d9e2b8027ef57784380aa54d
Khoo Jie Xuan <coolkhoo547@gmail.com>
2021-05-20 19:39:00 +0800

    Home button

--- esdm-student-mobile/src/app/dining/dining.page.ts
+++ esdm-student-mobile/src/app/dining/dining.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'; 
 
 @Component({
   selector: 'app-dining',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DiningPage implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {
   }
+
+  backHome(){
+  	this.router.navigate(['./home'])
+  }
+
+  // backHome() {
+  //   this.route.navigate(['./home']);
+  // }
 
 }
