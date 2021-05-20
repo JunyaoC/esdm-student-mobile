4bbabfb828d90b565836ff0481e7314000b0fc3e
Junyao Chan <junyaoxiandingchan@gmail.com>
2021-05-20 09:38:14 +0800

    Update home.page.ts

--- esdm-student-mobile/src/app/home/home.page.ts
+++ esdm-student-mobile/src/app/home/home.page.ts
@@ -23,14 +23,14 @@ export class HomePage {
 		route:"library"
 	},
 	{
-		iconPath:"assets/icon/health.svg",
-		label:"Health",
-		route:"health"
-	},
-	{
 		iconPath:"assets/icon/attendance.svg",
 		label:"Attendance",
 		route:"attendance"
+	},
+	{
+		iconPath:"assets/icon/health.svg",
+		label:"Health",
+		route:"health"
 	},
 	{
 		iconPath:"assets/icon/attendance.svg",
