53b793ba6cafa9cefb83b6bde313ace07d793feb
Jiakmai <71082196+Jiakmai@users.noreply.github.com>
2021-05-22 02:00:59 +0800

    lala

--- esdm-student-mobile/src/app/hostel/registration/open-registration/open-registration.page.ts
+++ esdm-student-mobile/src/app/hostel/registration/open-registration/open-registration.page.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
+import { Router } from '@angular/router'; 
+import axios from 'axios';
 
 @Component({
   selector: 'app-open-registration',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OpenRegistrationPage implements OnInit {
 
-  constructor() { }
+	server : string = 'http://localhost/php-folder/';
+  restaurant_records:any = [];
+
+  constructor(private router:Router) { }
 
   ngOnInit() {
+  	this.fetchRestaurant(0);
+  }
+  fetchRestaurant(event){
+    let body = {
+      action:'list_restaurant',
+    }
+
+    axios.post(this.server + 'hostel/open-registration.php', JSON.stringify(body)).then((res:any) => {
+      this.restaurant_records = [...res.data.restaurant]
+
+      console.log(res);
+
+      if(event != 0){
+        event.target.complete();
+      }
+    })
+
   }
 
 }
