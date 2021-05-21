23e972232b4708d9d313cca765080310a2156345
TheresaKho <theresakho1234@gmail.com>
2021-05-21 11:59:59 +0800

    Update vehicle.page.ts

--- esdm-student-mobile/src/app/vehicle/vehicle.page.ts
+++ esdm-student-mobile/src/app/vehicle/vehicle.page.ts
@@ -29,7 +29,7 @@ export class VehiclePage implements OnInit {
   	this.router.navigate(['./vehicle/history'])
   }
 
-  goviewhistory(){
+  goviewticket(){
   	this.router.navigate(['./vehicle/ticket'])
   }
 
