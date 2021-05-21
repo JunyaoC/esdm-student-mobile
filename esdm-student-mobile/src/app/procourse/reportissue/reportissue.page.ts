1482b6f08f942cfe510125e6767a178da8ef8731
jia7lim <jia7lim@users.noreply.github.com>
2021-05-21 13:06:41 +0800

    form validate

--- esdm-student-mobile/src/app/procourse/reportissue/reportissue.page.ts
+++ esdm-student-mobile/src/app/procourse/reportissue/reportissue.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-reportissue',
@@ -8,10 +11,25 @@ import { Router } from '@angular/router';
 })
 export class ReportissuePage implements OnInit {
 
-  constructor(private router:Router) { }
-
-  ngOnInit() {
+  loginForm: FormGroup;
+  constructor(private fb: FormBuilder,private router:Router) {
   }
+
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  initForm(): void {
+	  this.loginForm = this.fb.group({
+	      matric: ['',  [Validators.required,
+      Validators.pattern('[A-Z0-9]+'), Validators.maxLength(9)]],
+	      title: ['', Validators.required]
+	});
+  }
+  isValidInput(fieldName): boolean {
+    return this.loginForm.controls[fieldName].invalid &&
+      (this.loginForm.controls[fieldName].dirty || this.loginForm.controls[fieldName].touched);
+}
 
   backProcourse(){
   	this.router.navigate(['./procourse'])
