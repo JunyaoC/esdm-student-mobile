07ea8dcef5261e78f91850bc662300995139e2c3
TheresaKho <theresakho1234@gmail.com>
2021-05-21 22:22:14 +0800

    Retrieve

--- esdm-student-mobile/src/app/vehicle/history/history.page.ts
+++ esdm-student-mobile/src/app/vehicle/history/history.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import axios from 'axios'; 
 
 @Component({
   selector: 'app-history',
@@ -6,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./history.page.scss'],
 })
 export class HistoryPage implements OnInit {
-
-  constructor() { }
+server : string = "http://localhost/php-folder/";
+sticker_records:any =[];
+  constructor(private router: Router) { }
 
   ngOnInit() {
+    this.fetchSticker(0);
+  }
+
+  govehicle(){
+  	this.router.navigate(['./vehicle'])
+  }
+
+  fetchSticker(event){
+    let body={
+      action: 'show_history',
+    }
+    axios.post(this.server+'vehicle/vehicle.php', JSON.stringify(body)).then((res:any)=>{
+      this.sticker_records = [...res.data.vehicle]
+
+       console.log(this.sticker_records);
+    })
+
   }
 
 }
