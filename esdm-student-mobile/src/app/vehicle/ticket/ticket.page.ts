f2a09276f429f021cbcf59bf1d9cd3b2554b7b71
Kathelene Yap <katheleneyap@gmail.com>
2021-05-21 22:55:15 +0800

    Add ticket page

--- esdm-student-mobile/src/app/vehicle/ticket/ticket.page.ts
+++ esdm-student-mobile/src/app/vehicle/ticket/ticket.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import axios from 'axios';
 
 @Component({
   selector: 'app-ticket',
@@ -7,14 +8,28 @@ import { Router } from '@angular/router';
   styleUrls: ['./ticket.page.scss'],
 })
 export class TicketPage implements OnInit {
-
+  server : string = "http://localhost/php-folder/";
+  ticket_records:any =[];
   constructor(private router: Router) { }
 
   ngOnInit() {
+    this.fetchTicket(0);
   }
 
   govehicle(){
   	this.router.navigate(['./vehicle'])
+  }
+
+  fetchTicket(event){
+    let body={
+      action: 'show_ticket',
+    }
+    axios.post(this.server+'vehicle/ticket.php', JSON.stringify(body)).then((res:any)=>{
+      this.ticket_records = [...res.data.ticket]
+
+       console.log(this.ticket_records);
+    })
+
   }
 
 }
