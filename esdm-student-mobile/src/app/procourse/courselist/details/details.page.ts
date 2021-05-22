6d681f9aba2095734162adbbcc957c4df95487fc
rouxuen <rouxuen0322@hotmail.com>
2021-05-22 13:18:43 +0800

    update details

--- esdm-student-mobile/src/app/procourse/courselist/details/details.page.ts
+++ esdm-student-mobile/src/app/procourse/courselist/details/details.page.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { AlertController } from '@ionic/angular';
 import {ActivatedRoute} from '@angular/router';
 import axios from 'axios';
+import { ThisReceiver } from '@angular/compiler';
 
 @Component({
   selector: 'app-details',
@@ -14,18 +15,19 @@ export class DetailsPage implements OnInit {
   server : string = 'http://localhost/php-folder/';
   procourse_list:any = [];
   code:string;
+  section_list = [];
+
+
   constructor(private router:Router,public alertController: AlertController,private activatedRoute: ActivatedRoute) { }
  
 
   ngOnInit() {
     this.activatedRoute.queryParams.subscribe(params => {
       let procourse_code = params['procourse_code'];
-      
-      console.log(procourse_code);
       this.code=procourse_code;
-      this.fetchCourselist(1);
+      this.fetchCourselist(0);
+      this.fetchSectionlist(0);
     });
- 
     
   }
 
@@ -33,16 +35,7 @@ export class DetailsPage implements OnInit {
   	this.router.navigate(['./procourse/courselist'])
   }
   
-  async presentAlertMultipleButtons() {
-    const alert = await this.alertController.create({
-      header: 'Design Thinking For Enterpreneur - Section 1',
-      message: 'Date : 30/5/2021<br>Location : UTM P20<br>Facilitator : ASSOC. PROF. DR ABDUL RASHID BIN HUSAIN',
-      buttons: ['Cancel', 'Register']
-    });
-
-    await alert.present();
-    
-  }
+  
   fetchCourselist(event){
     let body = {
       action:'list_procourse',
@@ -52,6 +45,22 @@ export class DetailsPage implements OnInit {
     axios.post(this.server + 'procourse/coursedetails.php', JSON.stringify(body)).then((res:any) => {
       this.procourse_list = [...res.data.procourse]
 
+      if(event != 0){
+        event.target.complete();
+      }
+    })
+
+  }
+
+  fetchSectionlist(event){
+    let body = {
+      action:'list_section',
+      id:this.code,
+    }
+
+    axios.post(this.server + 'procourse/coursedetails.php', JSON.stringify(body)).then((res:any) => {
+      this.section_list = [...res.data.section]
+
       console.log(res);
 
       if(event != 0){
@@ -59,6 +68,39 @@ export class DetailsPage implements OnInit {
       }
     })
 
+  }
+
+  async presentAlertMultipleButtons(section) {
+
+    const alert = await this.alertController.create({
+      header: this.procourse_list.procourse_code,
+      message: 'Date : 30/5/2021<br>Location : UTM P20<br>Facilitator : ASSOC. PROF. DR ABDUL RASHID BIN HUSAIN',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            console.log('Confirm Cancel: Cancel');
+          }
+        }, {
+          text: 'Register',
+          handler: () => {
+            this.registerCourse(section)
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+    
+  }
+
+
+  registerCourse(section){
+    console.log(section)
+
+    // axios.post('endpit')
   }
   
 }
