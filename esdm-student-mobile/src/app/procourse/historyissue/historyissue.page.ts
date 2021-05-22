3f07675c72e2fb2faf6251d8bfca2d01af956b30
jia7lim <jia7lim@gmail.com>
2021-05-22 23:30:51 +0800

    update report history

--- esdm-student-mobile/src/app/procourse/historyissue/historyissue.page.ts
+++ esdm-student-mobile/src/app/procourse/historyissue/historyissue.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 import axios from 'axios';
 
 
@@ -11,14 +12,48 @@ import axios from 'axios';
 export class HistoryissuePage implements OnInit {
 
 
-  constructor(private router:Router) { }
+  constructor(private router:Router,public alertController: AlertController) { }
   server : string = 'http://localhost/php-folder/';
+  list_issue:any = [];
+  id:any ="";
+
 
   ngOnInit() {
+  this.fetchIssuelist(0);
   }
 
   backProcourse(){
   	this.router.navigate(['./procourse/issue'])
   }
+
+  fetchIssuelist(event){
+    let body = {
+      action:'list_issue',
+    }
+
+    axios.post(this.server + 'procourse/reportissue.php', JSON.stringify(body)).then((res:any) => {
+      this.list_issue = [...res.data.issue]
+
+      console.log(res);
+
+      if(event != 0){
+        event.target.complete();
+      }
+    })
+
+  }
+
+  async details(issue){
+
+      const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Issue Answered',
+      message: `${issue.issue_answer}`,
+      buttons: ['Done']
+    });
+    await alert.present();
+  }
+
+
 
 }
