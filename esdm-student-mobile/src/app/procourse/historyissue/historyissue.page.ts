03284009a765140bdc85cb29378a18dbaaebcb6a
jia7lim <jia7lim@gmail.com>
2021-05-23 11:49:35 +0800

    refresher

--- esdm-student-mobile/src/app/procourse/historyissue/historyissue.page.ts
+++ esdm-student-mobile/src/app/procourse/historyissue/historyissue.page.ts
@@ -17,6 +17,14 @@ export class HistoryissuePage implements OnInit {
   list_issue:any = [];
   id:any ="";
 
+   doRefresh(event) {
+    console.log('Begin async operation');
+
+    setTimeout(() => {
+      console.log('Async operation has ended');
+      event.target.complete();
+    }, 2000);
+  }
 
 
 
