127c4940f91de1cd789e778d2f905352167e8f86
Jiakmai <71082196+Jiakmai@users.noreply.github.com>
2021-05-24 13:06:30 +0800

    upgrade register

--- esdm-student-mobile/src/app/hostel/registration/registration.page.ts
+++ esdm-student-mobile/src/app/hostel/registration/registration.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import axios from 'axios';
 
 @Component({
   selector: 'app-registration',
@@ -8,9 +9,26 @@ import { Router } from '@angular/router';
 })
 export class RegistrationPage implements OnInit {
 
+  server : string = 'http://localhost/php-folder/';
+  phase_info:any = [];
+
   constructor(private route: Router) { }
 
   ngOnInit() {
+    this.CheckPhase();
+  }
+  CheckPhase(){
+    let body = {
+      action:'phase_period',
+    }
+
+    axios.post(this.server + 'hostel/phase.php', JSON.stringify(body)).then((res:any) => {
+
+      this.phase_info = [...res.data.detail]
+      console.log(res);
+
+    })
+
   }
   kuotaPage() {
     this.route.navigate(['hostel/registration/kuota-pengetua']);
